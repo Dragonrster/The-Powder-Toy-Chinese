8b001f464eb980fe7475bc040ab7016654bfbaf3
Dragonrster <2380723764@qq.com>
2026-01-14 14:07:23 +0800

    392

--- .github/upload-release-asset.sh
+++ .github/upload-release-asset.sh
@@ -6,7 +6,7 @@ mkdir $temp
 cp $ASSET_PATH $temp/$ASSET_NAME
 (
 	cd $temp
-	gh release upload $GITHUB_REF_NAME $ASSET_NAME
+	gh release upload $GITHUB_REF_NAME $ASSET_NAME --clobber
 )
 rm -r $temp
 echo browser_download_url=https://github.com/$GITHUB_REPOSITORY/releases/download/$GITHUB_REF_NAME/$ASSET_NAME >> $GITHUB_OUTPUT
