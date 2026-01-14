4be3415ba7d88d1f456ab59acdc3a2ad328d98dc
Dragonrster <2380723764@qq.com>
2026-01-14 16:25:54 +0800

    392

--- .github/upload-release-asset.sh
+++ .github/upload-release-asset.sh
@@ -6,14 +6,7 @@ mkdir $temp
 cp $ASSET_PATH $temp/$ASSET_NAME
 (
 	cd $temp
-	# 先创建 Release（直接发布）
-gh release create $GITHUB_REF_NAME $ASSET_NAME \
-    --title "$RELEASE_NAME" \
-    --notes "自动发布的版本" \
-    --clobber \
-    --draft=false \
-    --prerelease=false
-
+	gh release upload $GITHUB_REF_NAME $ASSET_NAME --clobber 
 )
 rm -r $temp
 echo browser_download_url=https://github.com/$GITHUB_REPOSITORY/releases/download/$GITHUB_REF_NAME/$ASSET_NAME >> $GITHUB_OUTPUT
